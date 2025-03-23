52cba23fdd93913bf2a2957e1e9515d1b7cd4df0
davidwolfpaw <david@davidwolfpaw.com>
2025-03-22 21:02:20 -0400

    fixes indentation

--- assets/js/blocks/note-title/index.js
+++ assets/js/blocks/note-title/index.js
@@ -1,37 +1,37 @@
 (function(wp) {
-	const { registerBlockType } = wp.blocks;
-	const { RichText, MediaUpload, useBlockProps } = wp.blockEditor;
-	const el = wp.element.createElement;
+  const { registerBlockType } = wp.blocks;
+  const { RichText, MediaUpload, useBlockProps } = wp.blockEditor;
+  const el = wp.element.createElement;
 
-	registerBlockType('digital-garden/note-title', {
-			title: 'Title',
-			icon: 'edit',
-			category: 'widgets',
-			parent: [ 'digital-garden/note-block' ],
+  registerBlockType('digital-garden/note-title', {
+      title: 'Title',
+      icon: 'edit',
+      category: 'widgets',
+      parent: [ 'digital-garden/note-block' ],
 
-			attributes: {
-					content: { type: 'string' }
-			},
+      attributes: {
+          content: { type: 'string' }
+      },
 
-			edit: function(props) {
-					const { attributes, setAttributes } = props;
-					const blockProps = useBlockProps();
+      edit: function(props) {
+          const { attributes, setAttributes } = props;
+          const blockProps = useBlockProps();
 
-					return el(
-							'div',
-							blockProps,
-							el( RichText, {
-									tagName: 'div',
-									className: 'digital-garden-note-title',
-									value: attributes.content || '',
-									onChange: ( newContent ) => setAttributes( { content: newContent } ),
-									placeholder: 'Enter Title...'
-							})
-					);
-			},
+          return el(
+              'div',
+              blockProps,
+              el( RichText, {
+                  tagName: 'div',
+                  className: 'digital-garden-note-title',
+                  value: attributes.content || '',
+                  onChange: ( newContent ) => setAttributes( { content: newContent } ),
+                  placeholder: 'Enter Title...'
+              })
+          );
+      },
 
-			save: function() {
-					return null;
-			}
-	});
+      save: function() {
+          return null;
+      }
+  });
 })(window.wp);
