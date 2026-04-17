7a4864718436ad73e15d3b212fc5012072140911
davidwolfpaw <david@davidwolfpaw.com>
2026-04-17 19:29:27 -0400

    fixes rendering of block on plugin activation

--- digital-garden.php
+++ digital-garden.php
@@ -65,10 +65,52 @@ function digital_garden_activate() {
 	$page_id = get_option( 'digital_garden_page_id' );
 
 	if ( ! $page_id || ! get_post( $page_id ) ) {
+		$page_content = <<<'BLOCK_CONTENT'
+<!-- wp:digital-garden/container -->
+<div class="wp-block-digital-garden-container"><!-- wp:digital-garden/garden-title /-->
+
+<!-- wp:digital-garden/tag-filter /-->
+
+<!-- wp:group {"className":"digital-garden-archive-filters","layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group digital-garden-archive-filters"><!-- wp:digital-garden/active-filter /-->
+
+<!-- wp:digital-garden/completeness-filter /-->
+
+<!-- wp:digital-garden/search /--></div>
+<!-- /wp:group -->
+
+<!-- wp:digital-garden/note-block -->
+<div class="wp-block-digital-garden-note-block"><!-- wp:digital-garden/note-title -->
+<h3 style="text-align:left" class="wp-block-digital-garden-note-title">Example Note Title</h3>
+<!-- /wp:digital-garden/note-title -->
+
+<!-- wp:digital-garden/note-content -->
+<div style="text-align:left" class="wp-block-digital-garden-note-content">Note Content (full post content)</div>
+<!-- /wp:digital-garden/note-content -->
+
+<!-- wp:digital-garden/note-tags /-->
+
+<!-- wp:digital-garden/note-completeness /-->
+
+<!-- wp:digital-garden/note-featured-image /-->
+
+<!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:digital-garden/note-publish-date /--></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:digital-garden/note-modify-date /--></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:digital-garden/note-block --></div>
+<!-- /wp:digital-garden/container -->
+BLOCK_CONTENT;
+
 		$page = array(
 			'post_title'   => 'Digital Garden',
 			'post_name'    => 'garden',
-			'post_content' => '<!-- wp:digital-garden/container /-->',
+			'post_content' => $page_content,
 			'post_status'  => 'publish',
 			'post_type'    => 'page',
 			'post_author'  => 1,
